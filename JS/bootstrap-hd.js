e891854f48de778d8f2af2984559eb41d444d7d1
Sebastian Schultz <sebastian.e.schultz@gmail.com>
2018-03-29 23:24:51 -0400

    .

--- JS/bootstrap-hd.js
+++ JS/bootstrap-hd.js
@@ -1,4 +1,11 @@
-<!---night mode! -->
+<!---night mode! jquery finds/selects the .onbutton element (the "on" button for night mode); action "click" selects the "p" element (alternately the "jumbotron" or "body") and changes the css to make the text white and background for the body and jumbotron black-->
+
+$(".onbutton").click(function() {
+  $("h1").css("color", "white");
+});
+$(".onbutton").click(function() {
+  $("h2").css("color", "white");
+});
 $(".onbutton").click(function() {
   $("p").css("color", "white");
 });
@@ -8,11 +15,28 @@ $(".onbutton").click(function() {
 $(".onbutton").click(function() {
   $("body").css("background-color", "black");
 });
+$(".onbutton").click(function() {
+  $(".table").css("color", "#ff8080");
+});
 
-<!-- undo night mode --!>
+$(".onbutton").click(function() {
+  $(".footnotes").css("background-color", "black");
+});
+
+
+
+<!-- undo night mode! jquery finds/selects the .offbutton element (to undo the styling of the night mode); action "click" selects the "p", "jumbotron" and "body" elements and removes the style attributes -->
 
 $(".offbutton").click(function() {
 $(".jumbotron").removeAttr("style");
+});
+
+$(".offbutton").click(function() {
+$(".footnotes").removeAttr("style");
+});
+
+$(".offbutton").click(function() {
+$(".table").removeAttr("style");
 });
 
 $(".offbutton").click(function() {
@@ -21,4 +45,12 @@ $("p").removeAttr("style");
 
 $(".offbutton").click(function() {
 $("body").removeAttr("style");
+});
+
+$(".offbutton").click(function() {
+$("h1").removeAttr("style");
+});
+
+$(".offbutton").click(function() {
+$("h2").removeAttr("style");
 });
